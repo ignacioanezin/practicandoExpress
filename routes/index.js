8661098801013e95a5986f1d83d5486d301a8d2b
Igna Anezin <62267176+ignacioanezin@users.noreply.github.com>
2021-06-29 21:16:35 -0300

    Building pages with ejs and express

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/contact', (req, res) => {
+  res.render('pages/contact');  
+});
+
+router.get('/about', (req, res) => {
+  res.render('pages/about');  
 });
 
 module.exports = router;
