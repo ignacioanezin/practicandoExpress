5cbdc110464d9f243c6bf8f97e8a239df98e11bc
Igna Anezin <62267176+ignacioanezin@users.noreply.github.com>
2021-07-01 20:21:55 -0300

    Config Sequelize Data Base

---
+++ models/author.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+    const Author = sequelize.define('author', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+
+        },
+        completeName: DataTypes.STRING(100)
+    },
+    {
+        freezeTableName: true,
+        timeStamp: false,
+    });
+
+    Author.associate = (models) => {
+        Author.hasMany(models.book);
+    }   
+
+    return Author;
+}
---
+++ models/book.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+    const Book = sequelize.define('book', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+
+        },
+        title: DataTypes.STRING(200),
+        price: DataTypes.INTEGER,
+        cover: DataTypes.STRING(150)
+        
+    },
+    {
+        freezeTableName: true,
+        timeStamp: false,
+    });
+
+    Book.associate = (models) => {
+        Book.belongsTo(models.author)
+    }
+
+    return Book;
+}
